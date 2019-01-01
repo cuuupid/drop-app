660fd5c56734cfcf680f564fa3d0b25ed6539172
shlokkh <shlok@shlok.net>
2019-01-01 01:38:33 -0500

    basic login page

--- client/index.js
+++ client/index.js
@@ -1,11 +1,24 @@
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'hello'
+        appname: 'Unnamed Bill Sharing App',
+        message: 'hello',
+        venmo_username: ''
+    },
+    created: function() {
+        if(localStorage.getItem("venmo")) {
+           //Redirect to landing page 
+        } else {
+            //Redirect to login page
+        }
     },
     methods: {
         sayHello: function () {
             this.message = 'hello world!'
+        },
+        login: function() {
+            localStorage.setItem('venmo', this.venmo_username)
+            //Should redirect to landing page
         }
     }
 })
